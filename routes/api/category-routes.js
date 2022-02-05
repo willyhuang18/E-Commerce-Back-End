4bac541f9bade7fe36de65acd28a23a2f975064a
wiily <willyhuang18@gmail.com>
2022-02-05 14:55:58 -0800

    create function for post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,6 +41,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(response => res.json(response))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
