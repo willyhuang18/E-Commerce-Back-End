8b8c52331e2d97a7ebbfc6b3e9d47b815b8fead9
wiily <willyhuang18@gmail.com>
2022-02-05 14:36:56 -0800

    find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,14 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
