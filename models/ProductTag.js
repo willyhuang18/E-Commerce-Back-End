f8eea870adf68a9a4b63834b9d70050daaddb3b7
wiily <willyhuang18@gmail.com>
2022-02-04 22:36:07 -0800

    references to the product model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references to the product model 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
