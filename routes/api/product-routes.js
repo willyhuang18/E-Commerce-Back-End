b045a283b2e3aff48ab666824ee3b5fd9916c648
wiily <willyhuang18@gmail.com>
2022-02-05 20:35:02 -0800

    fixed the bug for that

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -12,8 +12,12 @@ router.get('/', (req, res) => {
       {
         model: Category,
         attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
       }
-      
+
     ]
   })
     .then(response => res.json(response))
