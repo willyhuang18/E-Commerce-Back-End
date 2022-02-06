1f81fd06839e818d87f03dec280e68c794411723
wiily <willyhuang18@gmail.com>
2022-02-05 20:30:31 -0800

    find one function for get route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,6 +26,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      }
+    ]
+  })
+    
 });
 
 // create new product
