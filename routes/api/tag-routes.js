08aa9a4313febaf628e5032975adb3f3fad6ddef
wiily <willyhuang18@gmail.com>
2022-02-05 20:47:41 -0800

    adding the destory method

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -82,6 +82,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+        id: req.params.id
+    }
+  })
 });
 
 module.exports = router;
