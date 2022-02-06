d9745b2e68423fc0c4c66193b673fc1a9923a573
wiily <willyhuang18@gmail.com>
2022-02-05 20:43:38 -0800

    adding tag find all

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
